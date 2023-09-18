92331b5237f7563d87488852a68ba0fc689c7f3c
Stephen Hove <stephenh@digitalcabinet.co.za>
2023-09-18 21:00:12 +0200

    Updating website footers

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -21,7 +21,17 @@ export default function Footer() {
       <Container className="d-flex flex-column">
         <ul className="footer__nav d-flex flex-wrap">
           
-            <Col xs={4} md={4} lg={2}>
+            <Col xs={1} md={2} lg={1}>
+                <li className="footer__item">
+                    <a className="footer__link" 
+                    
+                    >
+                    
+                    </a>
+                </li>
+            </Col>
+
+            <Col xs={3} md={3} lg={2}>
                 <li className="footer__item">
                     <a className="footer__link" 
                     onClick={() => scrollToSection("about")}
@@ -32,7 +42,7 @@ export default function Footer() {
                 </li>
             </Col>
 
-            <Col xs={4} md={4} lg={2}>
+            <Col xs={4} md={3} lg={2}>
                 <li className="footer__item">
                     <a className="footer__link" a href="/careers">
                     Careers
