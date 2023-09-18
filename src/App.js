4fea662d644c879492b4b3bf4121d406763d8b95
Stephen Hove <stephenh@digitalcabinet.co.za>
2023-09-18 12:34:54 +0200

    Adding Nav Bar, welcome text and background image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import PageTitleUpdater from "./Pages/PageTitleUpdater";
+import Home from "./Pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Services from "./Pages/Services";
+import Projects from "./Pages/Projects";
+import Team from "./Pages/Team";
+import Footer from "./Pages/Footer";
+import Contact from "./Pages/Contact";
+import Gallery from "./Pages/Gallery";
+import Careers from "./Pages/Careers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Mabocap with React
-        </p>
-      </header>
-    </div>
+    <BrowserRouter>
+      <PageTitleUpdater />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
